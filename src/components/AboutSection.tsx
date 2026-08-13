201eb726a2ad93426cda06609ccc42feda4f334a
Sanila Chowdhury <sanilachowdhury@gmail.com>
2026-08-13 08:36:26 -0400

    removed math minor

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -26,11 +26,11 @@ const AboutSection = () => {
             <div className="space-y-3 text-portfolio-navy leading-relaxed text-base">
               <p>
                 I'm an undergrad at CUNY Hunter College studying Computer
-                Science with a minor in Mathematics. I'm an aspiring software
-                engineer with a focus on full-stack development. My journey in
-                tech began in high school with a four-year software engineering
-                course where I discovered my passion for programming and turning
-                my ideas into products that can shape people's lives.
+                Science. I'm an aspiring software engineer with a focus on
+                full-stack development. My journey in tech began in high school
+                with a four-year software engineering course where I discovered
+                my passion for programming and turning my ideas into products
+                that can shape people's lives.
               </p>
               <p>
                 I'm someone who embraces new challenges and isn't afraid to take
